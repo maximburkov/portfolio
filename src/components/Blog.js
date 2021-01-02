bc737f6b6414844364c1892b273d795a627da028
maximburkov <maxburkov12@gmail.com>
2021-01-02 18:57:30 +0300

    Sample posts showed in blog section.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import Post from './Post'
 
 export default class Blog extends Component {
+    constructor(props) {
+        super(props);
+        
+        var posts = [
+            {
+                "title" : "Shocking news",
+                "text" : "It was yesterday..."
+            },
+            {
+                "title" : "Good news",
+                "text" : "Baby tiger was born in the zoo."
+            }
+        ];
+
+        this.state = {
+            posts: posts
+        };
+      }
+
+    componentDidMount() {
+
+    }
+    
+
     render() {
+        const postsToRender = this.state.posts.map((post) => 
+            <Post title={post.title} text={post.text}/>);
+
         return (
             <div>
                 <h1>Blog</h1>
+                {postsToRender}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App text = "Maxim" />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
